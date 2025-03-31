1449e740990671dfdcd9fa6cce0830e03cc7b322
Sauhardya Haldar <imsauhardya@gmail.com>
2025-03-31 16:28:54 +0530

    Added a new feature to search syllabus of specific course of any department, changed the function based code structure of bot.py to class and method based structure, updated app.py accordingly, updated notices.json with xtracter

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import bot
+import bot as BotBrain
 
 app = Flask(__name__)
 CORS(app)
+bot = BotBrain.GeneralFunctions()
+bot.activateBot()
 
 @app.route('/data/message', methods=['POST'])
 def receive_message():
@@ -14,12 +16,12 @@ def receive_message():
     else:
         print(f"Received message: {query}")
 
-    try:
-        response = bot.reply(query)
-        return jsonify({"reply": response})
-    except Exception as e:
-        print("Error:",e)
-        return jsonify({"reply": "Sorry we ran into some INTERNAL SERVER ERROR! Please try again later"})
+    # try:
+    response = bot.reply(query)
+    return jsonify({"reply": response})
+    # except Exception as e:
+    #     print("Error:",e)
+    #     return jsonify({"reply": "Sorry we ran into some INTERNAL SERVER ERROR! Please reload the page or try again later"})
 
 
 if __name__ == "__main__":
--- server/bot.py
+++ server/bot.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import json
 from dateutil import parser
 from fuzzywuzzy import fuzz
+import re
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -58,166 +59,224 @@ model = genai.GenerativeModel(
 
         If user asks about general knowledge of a particular department reply strictly in the format 'ABOUT: FACULTY_NAME/DEPARTMENT_NAME'
         If user asks about courses of a particular department reply strictly in the format 'COURSES: FACULTY_NAME/DEPARTMENT_NAME'
-        If user asks about syllabus or curriculum of a particular course of a department reply strictly in the format 'SYLLABUS: FACULTY_NAME/DEPARTMENT_NAME/COURSE_NAME'
+        If user asks about the syllabus or courses of some particular course like ug, pg, bachelor, master, extra departmental, phd, doctorate, doctoral, diploma, certificate course etc. but doesn't tell the department, ask them the department and if they tell the department too then reply strictly in the format 'SYLLABUS: FACULTY_NAME/DEPARTMENT_NAME|COURSE_NAME'
   '''
 )
 
+class GeneralFunctions():
+    def activateBot(self):
+        self.bot = BotFunctions()
+        
+    def reply(self, query):
+        global chatHistory
+        chat_session = model.start_chat(
+            history=chatHistory
+        )
+        time.sleep(2)
+        response = chat_session.send_message(query)
+        output = response.text.strip()
 
-def read_json(fileJSON):
-    if os.path.exists(fileJSON):
-        with open(fileJSON,"r",encoding="utf-8") as f:
-            try:return json.load(f)
-            except:pass
-    return []
+        if "NOTICE_DATE: " in output:
+            output = self.bot.fetch_notice(date=output.replace("NOTICE_DATE: ",''))
+        elif "NOTICE: " in output:
+            output = self.bot.fetch_notice(keyword=output.replace("NOTICE: ",''))
+        elif "NOTICE_RECENT_MORE" == output:
+            output = self.bot.fetch_notice(more=True)
+        elif "NOTICE_RECENT" == output:
+            output = self.bot.fetch_notice()
+        elif "ABOUT: " in output:
+            output = self.bot.fetch_about(path=output.replace("ABOUT: ",'').lower())
+        elif "COURSES: " in output:
+            output = self.bot.fetch_courses(path=output.replace("COURSES: ",'').lower())
+        elif "SYLLABUS: " in output:
+            output = self.bot.fetch_syllabus(*output.replace("SYLLABUS: ",'').lower().split('|'))
 
-def restart():
-    global chatHistory
-    global notices, recent_notice_fetched
-    chatHistory = [
-        {
-            "role": "model",
-            "parts": ["Hi I am ChatJU! How can I help you today?"]
-        }
-    ]
-    notices = read_json("../xtracter/data/notices/notices.json")
-    recent_notice_fetched = 0
 
-def fetch_notice(date=None,keyword=None,more=False):
-    global notices, recent_notice_fetched
-    noticeRelevant = []
+        chatHistory.extend([{"role":"user","parts":[query]}, {"role":"model","parts":[output]}])
+        if len(chatHistory)>40:
+            chatHistory.pop(0)
+            chatHistory.pop(0)
+        return output
 
-    if date:
-        for notice in notices:
-            text = notice['title'].replace('-',' ').replace(':',' ').replace('=',' ')
-            if "07.03.2025" in text:print(text)
-            words = text.split()
-            extracted_dates = []
-            for word in words:
-                try:
-                    parsed_date = parser.parse(word, fuzzy=False, dayfirst=True)
-                    extracted_dates.append(parsed_date)
-                except:
-                    pass
-            parsed_date = parser.parse(date, fuzzy=False, dayfirst=True)
+    def read_json(self, fileJSON):
+        if os.path.exists(fileJSON):
+            with open(fileJSON,"r",encoding="utf-8") as f:
+                try:return json.load(f)
+                except:pass
+        return []
 
-            if parsed_date in extracted_dates:
-                noticeRelevant.append(notice)
-                if len(noticeRelevant)>10:
-                    break
+    def restart(self):
+        global chatHistory
+        global notices, recent_notice_fetched
+        chatHistory = [
+            {
+                "role": "model",
+                "parts": ["Hi I am ChatJU! How can I help you today?"]
+            }
+        ]
+        notices = self.read_json("../xtracter/data/notices/notices.json")
+        recent_notice_fetched = 0
 
-        output = "Here are few notices related to the given date:\n"
-        drawback = "Sorry, I can't find any notice corresponding to the mentioned date."
-
-    elif keyword:
-        for notice in notices:
-            threshold = 0.8
-            if (fuzz.partial_ratio(keyword.lower(), notice['title'].lower()) > threshold):
-                noticeRelevant.append(notice)
-                if len(noticeRelevant)>10:
-                    break
-
-        output = "Here are few notices matching your search:\n"
-        drawback = "Sorry, I can't find any notice corresponding to your search."
-
-    else:
-        if more:
-            recent_notice_fetched += 10
-            output = "Here are some few more recent notices:\n"
-        else:
-            recent_notice_fetched = 10
-            output = "Here are some few recent notices:\n"
-
-        noticeRelevant = notices[recent_notice_fetched-10:recent_notice_fetched]
-        drawback = "Sorry, no more notices found!"
-    
-    if noticeRelevant:
-        for (i, notice) in enumerate(noticeRelevant):
-            output += f"\n\n<code>{i+1}.</code> <b>{notice['title']}</b>\n\
-                <span>🔗 <a target='_blank' href={notice['link']}>{notice['link']}</a></span>\n"
-            if notice['docs']:
-                output += "Embedded Resources in the link:"
-                for docs in notice['docs']:
-                    output += f"\n<span>🔗 <a target='_blank' href={docs['link']}>{docs['heading']}</a></span>"
-    else:
-        output = drawback
-    
-    return output
-
-def fetch_about(path):
-    if path:
-        data = read_json(f"../xtracter/data/syllabus/{path}/about.json")
-        output = f"<b>Department of {data['name']}</b>\n\n{data['description']}"
-    else:
-        output = "Sorry, can't find anything related to your question!"
-    return output
-
-def fetch_courses(path):
-    if path:
-        name = read_json(f"../xtracter/data/syllabus/{path}/about.json")['name']
-        courses = read_json(f"../xtracter/data/syllabus/{path}/courses.json")
-        output = f"The list of courses available in the <b>Department of {name}</b> are as follows:"
-
+    def convertCoursesToHTML(self, courses):
+        code = ""
         for course in courses:
             duration = course['duration']
             intake = course['intake']
             syllabus = course['syllabus']
             curriculum = course['curriculum']
             absent_msg = "N/A"
-            output += f"\n\n<b><code>{course['course']}</code></b>\n<b>Duration:</b> {duration if duration else absent_msg}\n<b>Intake:</b> {intake if intake else absent_msg}\n"
+            code += f"\n\n<b><code>{course['course']}</code></b>\n<b>Duration:</b> {duration if duration else absent_msg}\n<b>Intake:</b> {intake if intake else absent_msg}\n"
 
-            output += "<b>Syllabus:</b> "
+            code += "<b>Syllabus:</b> "
             if len(syllabus)>1:
                 for link in syllabus:
-                    output += f"\n● <a href='{link['link']}' target='_blank'>{link['title']}</a>"
+                    code += f"\n● <a href='{link['link']}' target='_blank'>{link['title']}</a>"
             elif len(syllabus):
-                output += f"<a href='{syllabus[0]['link']}' target='_blank'>{syllabus[0]['title']}</a>"
+                code += f"<a href='{syllabus[0]['link']}' target='_blank'>{syllabus[0]['title']}</a>"
             else:
-                output+=absent_msg
+                code+=absent_msg
 
-            output += "\n<b>Curriculum:</b> "
+            code += "\n<b>Curriculum:</b> "
             if len(curriculum)>1:
                 for link in curriculum:
-                    output += f"\n● <a href='{link['link']}' target='_blank'>{link['title']}</a>"
+                    code += f"\n● <a href='{link['link']}' target='_blank'>{link['title']}</a>"
             elif len(curriculum):
-                output += f"<a href='{curriculum[0]['link']}' target='_blank'>{curriculum[0]['title']}</a>"
+                code += f"<a href='{curriculum[0]['link']}' target='_blank'>{curriculum[0]['title']}</a>"
             else:
-                output+=absent_msg
-    else:
-        output = "Sorry, can't find anything related to your question!"
-    return output
+                code+=absent_msg
+        return code
 
+    def convertNoticesToHTML(self, notices):
+        code=""
+        for (i, notice) in enumerate(notices):
+            code += f"\n\n<code>{i+1}.</code> <b>{notice['title']}</b>\n\
+                <span>🔗 <a target='_blank' href={notice['link']}>{notice['link']}</a></span>\n"
+            if notice['docs']:
+                code += "Embedded Resources in the link:"
+                for docs in notice['docs']:
+                    code += f"\n<span>🔗 <a target='_blank' href={docs['link']}>{docs['heading']}</a></span>"
+        return code
 
-def reply(query):
-    global chatHistory
-    chat_session = model.start_chat(
-        history=chatHistory
-    )
-    time.sleep(2)
-    response = chat_session.send_message(query)
-    output = response.text.strip()
-    # print(output)
+    def convrtAboutToHTML(self, data):
+        return f"<b>Department of {data['name']}</b>\n\n{data['description']}"
 
-    if "NOTICE_DATE: " in output:
-        output = fetch_notice(date=output.replace("NOTICE_DATE: ",''))
-    elif "NOTICE: " in output:
-        output = fetch_notice(keyword=output.replace("NOTICE: ",''))
-    elif "NOTICE_RECENT_MORE" == output:
-        output = fetch_notice(more=True)
-    elif "NOTICE_RECENT" == output:
-        output = fetch_notice()
-    elif "ABOUT: " in output:
-        output = fetch_about(path=output.replace("ABOUT: ",'').lower())
-    elif "COURSES: " in output:
-        output = fetch_courses(path=output.replace("COURSES: ",'').lower())
+class BotFunctions():
+    def __init__(self):
+        self.general = GeneralFunctions()
 
+    def fetch_notice(self, date=None, keyword=None, more=False):
+        global notices, recent_notice_fetched
+        noticeRelevant = []
 
-    chatHistory.extend([{"role":"user","parts":[query]}, {"role":"model","parts":[output]}])
-    if len(chatHistory)>40:
-        chatHistory.pop(0)
-        chatHistory.pop(0)
-    return output
+        if date:
+            for notice in notices:
+                text = notice['title'].replace('-',' ').replace(':',' ').replace('=',' ')
+                if "07.03.2025" in text:print(text)
+                words = text.split()
+                extracted_dates = []
+                for word in words:
+                    try:
+                        parsed_date = parser.parse(word, fuzzy=False, dayfirst=True)
+                        extracted_dates.append(parsed_date)
+                    except:
+                        pass
+                parsed_date = parser.parse(date, fuzzy=False, dayfirst=True)
+
+                if parsed_date in extracted_dates:
+                    noticeRelevant.append(notice)
+                    if len(noticeRelevant)>10:
+                        break
+
+            output = "Here are few notices related to the given date:\n"
+            drawback = "Sorry, I can't find any notice corresponding to the mentioned date."
+
+        elif keyword:
+            for notice in notices:
+                threshold = 0.8
+                if (fuzz.partial_ratio(keyword.lower(), notice['title'].lower()) > threshold):
+                    noticeRelevant.append(notice)
+                    if len(noticeRelevant)>10:
+                        break
+
+            output = "Here are few notices matching your search:\n"
+            drawback = "Sorry, I can't find any notice corresponding to your search."
+
+        else:
+            if more:
+                recent_notice_fetched += 10
+                output = "Here are some few more recent notices:\n"
+            else:
+                recent_notice_fetched = 10
+                output = "Here are some few recent notices:\n"
+
+            noticeRelevant = notices[recent_notice_fetched-10:recent_notice_fetched]
+            drawback = "Sorry, no more notices found!"
+        
+        if noticeRelevant:
+            output += self.general.convertNoticesToHTML(noticeRelevant)
+        else:
+            output = drawback
+        
+        return output
+
+    def fetch_about(self, path):
+        if path:
+            data = self.general.read_json(f"../xtracter/data/syllabus/{path}/about.json")
+            output = self.general.convrtAboutToHTML(data)
+        else:
+            output = "Sorry, can't find anything related to your question!"
+        return output
+
+    def fetch_courses(self, path):
+        if path:
+            name = self.general.read_json(f"../xtracter/data/syllabus/{path}/about.json")['name']
+            courses = self.general.read_json(f"../xtracter/data/syllabus/{path}/courses.json")
+            output = f"The list of courses available in the <b>Department of {name}</b> are as follows:"
+            output += self.general.convertCoursesToHTML(courses)
+        else:
+            output = "Sorry, can't find anything related to your question!"
+        return output
+
+    def fetch_syllabus(self, path, course_name):
+        isBachelor = lambda x: 'bachelor' in x.lower() or 'BA' in x or 'B.' in x
+        isMaster = lambda x: 'master' in x.lower() or x.startswith('M') or 'idc/mdc ' in x.lower()
+        isDoctor = lambda x: 'doctor' in x.lower() or 'Ph.D' in x
+        isExtraDept = lambda x: 'extra departmental' in x.lower();
+        isDiploma = lambda x: 'diploma' in x.lower() or 'PG ' in x
+
+        if path:
+            dept_name = self.general.read_json(f"../xtracter/data/syllabus/{path}/about.json")['name']
+            data = self.general.read_json(f"../xtracter/data/syllabus/{path}/courses.json")
+            COURSES = {'bachelor': [x for x in data if isBachelor(x['course'])],
+                    'master': [x for x in data if isMaster(x['course'])],
+                    'doctor': [x for x in data if isDoctor(x['course'])],
+                    'extra dept': [x for x in data if isExtraDept(x['course'])],
+                    'diploma': [x for x in data if isDiploma(x['course'])]}
+
+            regexBachelor = r"\b(bachelors?|b\.?a\.?|b\.?sc\.?|b\.?e\.?|b\.?tech\.?|u\.?g|under[^a-zA-Z0-9]*grad|under[^a-zA-Z0-9]*graduate|b\.?pharm|b\.?ed|b\.?p\.?ed)\b"
+            regexMaster = r"\b(masters?|m\.?a\.?|m\.?sc\.?|m\.?e\.?|m\.?tech\.?|m\.?pharm|m\.?phil|mca|pg|post[^a-zA-Z0-9]*graduate|post[^a-zA-Z0-9]*grad|inter[^a-zA-Z0-9]*disciplinary|multi[^a-zA-Z0-9]*disciplinary|idc|mdc)\b"
+            regexDoctor = r"\b(ph\.?d\.?|doctorate|doctoral|dphil|research\s?(?:program(?:me|mes|s)*|degrees?|course|stud(?:ies|y)|opportunit(?:ies|y)|fellowships?|scholarships?))\b"
+            regexExtraDept = r"\b(extra[^a-zA-Z0-9]*departmental[^a-zA-Z0-9]*?(?:courses?|subjects?|program(?:me|mes|s)*|electives?)*)\b"
+            regexDiploma = r"\b(diploma|pg\s?(?:certificate|cert)|post\s?graduate\s?(?:certificate|diploma))\b"
+
+            REGEX = {'bachelor':regexBachelor,'master':regexMaster,'doctor':regexDoctor,'extra dept':regexExtraDept,'diploma':regexDiploma}
+            relevant = []
+
+            for course, regex in REGEX.items():
+                if re.match(regex,course_name,re.IGNORECASE):
+                    relevant.extend(COURSES[course])
+
+            output = f"The list of courses, you asked for, available in the <b>Department of {dept_name}</b> are as follows:"
+            if relevant:
+                output += self.general.convertCoursesToHTML(relevant)
+            else:
+                output = f"No matching courses could be found in the Department of {dept_name}"
+        else:
+            output = "Sorry, can't find anything related to your question!"
+        return output
 
 if __name__ == "__main__":
-    restart()
+    generalObject = GeneralFunctions()
+    generalObject.restart()
     while ((query := input("User: ").strip()) != 'quit'):
-        print("Bot:",reply(query))
+        print("Bot:",generalObject.reply(query))
